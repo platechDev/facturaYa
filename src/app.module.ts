dcf2a0f28a5a9be528a2db1bde7d04539e61cb72
axlMax <axlyeyo2000@gmail.com>
2024-07-27 10:40:10 -0500

    feature/create the client section

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
