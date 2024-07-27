fa80782535c967d9cd277343b1ec283335fb8fd2
axlMax <axlyeyo2000@gmail.com>
2024-07-27 10:43:34 -0500

    feature/complete the get service

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('clients')
-export class ClientsController {}
+export class ClientsController {
+    
+    @Get('')
+    async getClients(){
+        return {
+            'name':'Diego Axsel',
+            'userName':'DiegoA',
+            'address':'carrera 5 #5 -33',
+            'email':'axlyeyo2000@gmail.com'
+        }
+    }
+}
